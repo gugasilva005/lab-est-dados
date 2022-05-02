768bc0fb61a061724285e71ac341cb29ad3ddc19
Cesar Rocha <cesarvasconcelos@copin.ufcg.edu.br>
2022-05-02 16:23:22 -0300

    Incluir comentários protótipos função

--- setup/testar_debug.c
+++ setup/testar_debug.c
@@ -1,12 +1,8 @@
 #include  <math.h>
 #include  <stdio.h>
 
-int foo(int x)
-{
-    int y = x + 10;
-    y++;
-    return y;
-}
+/* area dos prototipos de funcoes */ 
+int foo( int a, int c );
 
 int main  (void)
 {
@@ -14,8 +10,16 @@ int main  (void)
     double  y  =  pow  (2, 5);
     printf  ("A raiz quadrada de  9.0  é  %.2f\n",  x);
     printf  ("2 elevado a 5 é %.2f\n",  y);
-    int  k = foo(5);
+    int  k = foo(5, 6);
     int  j = k;
     j+= 1000;
     return  0;
+}
+
+/* area do corpo da funcao */
+int foo(int a, int c)
+{
+    int b= a + 10 + c;
+    b++;
+    return b;
 }
